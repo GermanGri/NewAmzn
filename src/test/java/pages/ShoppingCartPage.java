e509777c7ee4d18f9bfdcdc03e93253c36fdcd5d
Razin14 <german.gribanov@gmail.com>
2023-05-23 22:29:37 +0300

    Add ShoppingCartPage

---
+++ src/test/java/pages/ShoppingCartPage.java
@@ -0,0 +1,15 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class CartPage extends HomePage {
+
+    private static final String CART
+
+    WebDriver driver;
+
+    public CartPage(WebDriver driver) {
+        super(driver);
+    }
+
+}
